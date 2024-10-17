813e67f7fadffb887f077f3a647a4e89ebc9c7d4
Kittisak Saengsri <play_boy_kit@hotmail.com>
2024-10-17 14:30:30 +0700

    feat: add component navbar & herosection at app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,74 @@ import "./App.css";
 
 function App() {
   return (
-    <section>
-      <h1>My First React App on Vite Dev Kit</h1>
-    </section>
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="flex items-center justify-between py-4 px-32 bg-white border-b ">
+      <a href="/" className="text-2xl font-bold">
+        hh<span className="text-green-500">.</span>
+      </a>
+      <div className="md:flex space-x-9 hidden">
+        <a href="/login" className="px-9 py-2  rounded-full border">
+          Log in
+        </a>
+        <a
+          href="/signup"
+          className="px-8 py-2 bg-gray-900 text-white rounded-full hover:bg-gray-700 transition-colors"
+        >
+          Sign up
+        </a>
+      </div>
+      <button className="md:hidden">Menu</button>
+    </nav>
+  );
+}
+
+function HeroSection() {
+  return (
+    <main className="container px-4 py-8 lg:py-16 mx-auto">
+      <div className="flex flex-col lg:flex-row items-center">
+        <div className="lg:w-1/3 mb-8 lg:mb-0 lg:pr-8">
+          <h1 className="text-4xl text-right lg:text-5xl font-bold mb-4 text-gray-800">
+            Stay <br className="hidden lg:block" />
+            Informed, <br />
+            Stay Inspired,
+          </h1>
+          <p className="text-right text-base text-gray-500">
+            Discover a World of Knowledge at Your Fingertips. Your Daily Dose of
+            Inspiration and Information.
+          </p>
+        </div>
+        <img
+          src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+          alt="Person with a cat"
+          className="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0"
+        />
+        <div className="lg:w-1/3 lg:pl-8">
+          <h2 className="text-left text-xs font-semibold text-gray-500 mb-2">
+            -Author
+          </h2>
+          <h3 className="text-left text-2xl font-bold mb-4 text-gray-600">
+            Thompson P.
+          </h3>
+          <p className="text-left text-base text-gray-500 mb-4">
+            I am a pet enthusiast and freelance writer who specializes in animal
+            behavior and care. With a deep love for cats, I enjoy sharing
+            insights on feline companionship and wellness.
+          </p>
+          <p className="text-left text-gray-500">
+            When I&apos;m not writing, I spend time volunteering at my local
+            animal shelter, helping cats find loving homes.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
